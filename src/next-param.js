05a7c2c2a47856923a8d1df7eae730be0f56e7dd
afeiship <1290657123@qq.com>
2017-08-11 21:37:56 +0800

    issue: intaila

---
+++ src/next-param.js
@@ -0,0 +1,26 @@
+(function () {
+
+  var global = global || this || self || window;
+  var nx = global.nx || require('next-js-core2');
+  var CHAR_AND = '&';
+  var CHAR_EQUAL = '=';
+
+  nx.param = function (inObj) {
+    var arr = [];
+    var key, value, encodeValue;
+    for (key in inObj) {
+      value = inObj[key];
+      if (value != null) {
+        encodeValue = nx.isArray(value) ? value.join() : value;
+        arr.push(encodeURIComponent(key) + CHAR_EQUAL + encodeURIComponent(encodeValue));
+      }
+    }
+    return arr.join(CHAR_AND);
+  };
+
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.param;
+  }
+
+}());
